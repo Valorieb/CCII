c53219ff51c6e56f06ac613e29cb8b94e914bd4a
Valorieb <valoriebroderick@gmail.com>
2025-10-09 17:09:47 -0500

    Started home page

---
+++ scripts/date.js
@@ -0,0 +1,7 @@
+let footer = document.getElementById("footer-content");
+
+const today = new Date();
+const year = today.getFullYear();
+
+footer.innerHTML = `Capital City Illinois Indivisible | Springfield, IL | © ${year} <br>`;
+
